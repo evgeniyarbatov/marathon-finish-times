9e582fc3706e553f7f0d9c9a50a7a3e0fbfd550d
Evgeny Arbatov <arbatovevgeny@gmail.com>
2024-08-21 18:19:13 +0700

    Use config and cache URL responses

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
-URL = https://worldathletics.org/records/all-time-toplists/road-running/marathon/all/women/senior?regionType=world&page=1&bestResultsOnly=true&firstDay=1900-01-01&lastDay=2024-08-12&maxResultsByCountry=all&eventId=10229534&ageCategory=senior
+CONFIG_FILE = config.json
+OUTPUT_DIR = data
 
 VENV_PATH = ~/.venv/marathon-finish-times
-OUTPUT_DIR = data
 
 all: scrape
 
@@ -13,7 +13,7 @@ install: venv
 	pip install --disable-pip-version-check -q -r requirements.txt
 
 scrape: install
-	source $(VENV_PATH)/bin/activate && \
-	python3 scripts/scrape.py "$(URL)" $(OUTPUT_DIR)
+	@source $(VENV_PATH)/bin/activate && \
+	python3 scripts/scrape.py "$(CONFIG_FILE)" $(OUTPUT_DIR)
 
 .PHONY: venv install scrape
--- scripts/scrape.py
+++ scripts/scrape.py
@@ -2,34 +2,75 @@ import sys
 import requests
 import shutil
 import os
+import json
+import pickle
 
 import pandas as pd
 
 from bs4 import BeautifulSoup
 
+def cache(filename):
+    def decorator(func):
+        def wrapped(*args):
+            key = (args)
+            try:
+                hash(key)
+            except TypeError as e:
+                return func(*args)
+            try:
+                with open(filename, 'rb') as f:
+                    cache = pickle.load(f)
+            except Exception:
+                cache = {}
+            if key in cache:
+                return cache[key]
+            else:
+                value = func(*args)
+                if value is not None:
+                    cache[key] = value
+                    with open(filename, 'wb') as f:
+                        pickle.dump(cache, f)
+                    return value
+        return wrapped
+    return decorator
+
+@cache('cache/scrape.pickle')
+def get_url(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.content
+    else:
+        return None
+
 def main(args):
-    url = args[0]
+    config_file = args[0]
     output_dir = args[1]
 
     if os.path.exists(output_dir):
         shutil.rmtree(output_dir)
     os.makedirs(output_dir, exist_ok=True)
 
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    
-    table = soup.find('table')
-    headers = [header.text.strip() for header in table.find_all('th')]
+    with open(config_file, 'r') as file:
+        configs = json.load(file)
+        
+    for config in configs:
+        url = config['url']
 
-    rows = []
-    for row in table.find_all('tr'):
-        cells = row.find_all('td')
-        cells = [cell.text.strip() for cell in cells]
-        if len(cells) > 0:
-            rows.append(cells)
+        content = get_url(url)
+        soup = BeautifulSoup(content, 'html.parser')
+        
+        table = soup.find('table')
+        headers = [header.text.strip() for header in table.find_all('th')]
 
-    df = pd.DataFrame(rows, columns=headers)
-    df.to_csv(f'{output_dir}/records.csv', index=False)
+        rows = []
+        for row in table.find_all('tr'):
+            cells = row.find_all('td')
+            cells = [cell.text.strip() for cell in cells]
+            if len(cells) > 0:
+                rows.append(cells)
+
+        df = pd.DataFrame(rows, columns=headers)
+        df.to_csv(f'{output_dir}/records.csv', index=False)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
